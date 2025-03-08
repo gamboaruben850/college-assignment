8b04fdfc2c78312f70d5eb633daa9114bf0fc18b
gamboaruben850 <gamboaruben850@gmail.com>
2025-03-08 06:00:58 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    int x = 10;
+    if (x < 5) {
+      System.out.println("X is less than 5");
+    } else {
+      System.out.println("X is greater than or equal to 5");
+    }
+  }
+}
