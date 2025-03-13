4ae5cbe01b9b3286861512015f32c65dd2fcf8e0
gamboaruben850 <gamboaruben850@gmail.com>
2025-03-13 10:46:57 +0000

    Add 7.java

---
+++ src/7.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class CollegeAssignment{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter your name: ");
+    String name = input.nextLine();
+    System.out.println("Hello " + name);
+  }
+}
