c16bb84abe064a4877c1c90303121004f48f3083
gamboaruben850 <gamboaruben850@gmail.com>
2025-03-24 03:33:08 +0000

    Add 17.java

---
+++ src/17.java
@@ -0,0 +1,12 @@
+public class SimpleExample {
+    public static void main(String[] args) {
+        // Example 1: Basic print statement
+        System.out.println("Hello, World!");
+        
+        // Example 2: Looping through elements in an array
+        int[] numbers = {10, 20, 30};
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
