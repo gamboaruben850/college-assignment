2241c5b722b4f38b378db69d73fdda2afd727f7e
gamboaruben850 <gamboaruben850@gmail.com>
2025-05-25 14:24:32 +0000

    Add 42.java

---
+++ src/42.java
@@ -0,0 +1,16 @@
+public class SimpleCollegeAssignment {
+    public static void main(String[] args) {
+        // Example of a simple loop
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+        
+        // Another example of a for loop
+        String str = "Hello, World!";
+        for (int j = 0; j < str.length(); j++) {
+            System.out.print(str.charAt(j));
+        }
+    }
+}
